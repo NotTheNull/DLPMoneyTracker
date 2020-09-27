dcb1e0fe074b28602404cb0e6e5cdef0f3bc9522
NotTheNull <objnotnull@live.com>
2020-09-27 14:58:39 -0700

    Adding Debt Payment UI (somehow got ignored)

---
+++ DLPMoneyTracker/DataEntry/AddTransaction/AddDebtPayment.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DLPMoneyTracker.DataEntry.AddTransaction
+{
+    /// <summary>
+    /// Interaction logic for AddDebtPayment.xaml
+    /// </summary>
+    public partial class AddDebtPayment : Window
+    {
+        private AddDebtPaymentVM _viewModel;
+
+        public AddDebtPayment(AddDebtPaymentVM viewModel)
+        {
+            InitializeComponent();
+            _viewModel = viewModel;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.SavePayment();
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.Clear();
+            this.Close();
+        }
+    }
+}
