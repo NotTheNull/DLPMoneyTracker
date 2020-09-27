e50133dc6082bdf83c63d2ae8f58f1a507f7dae0
NotTheNull <objnotnull@live.com>
2020-09-26 17:24:17 -0700

    Initial Balances can now be added to Money Accounts.

---
+++ DLPMoneyTracker/DataEntry/AddTransaction/AddDebtPaymentVM.cs
@@ -0,0 +1,22 @@
+﻿using DLPMoneyTracker.Core;
+using DLPMoneyTracker.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DLPMoneyTracker.DataEntry.AddTransaction
+{
+    class AddDebtPaymentVM : BaseViewModel
+    {
+        private ILedger _ledger;
+        private ITrackerConfig _config;
+
+
+        public AddDebtPaymentVM(ILedger ledger, ITrackerConfig config)
+        {
+            _ledger = ledger;
+            _config = config;
+        }
+
+    }
+}
---
+++ DLPMoneyTracker/DataEntry/AddTransaction/AddTransactionVM.cs
@@ -0,0 +1,28 @@
+﻿using DLPMoneyTracker.Core;
+using DLPMoneyTracker.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DLPMoneyTracker.DataEntry.AddTransaction
+{
+    public class AddTransactionVM : BaseViewModel
+    {
+        private ILedger _ledger;
+        private ITrackerConfig _config;
+
+
+
+
+
+
+
+
+        public AddTransactionVM(ILedger ledger, ITrackerConfig config)
+        {
+            _ledger = ledger;
+            _config = config;
+        }
+
+    }
+}
