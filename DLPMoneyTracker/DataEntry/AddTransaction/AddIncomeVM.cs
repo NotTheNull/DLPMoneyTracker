cd9361e2d109e6ac6c14fa0e7b6adfd53b5addf0
NotTheNull <objnotnull@live.com>
2020-09-27 18:41:28 -0700

    Created Add Income UI

---
+++ DLPMoneyTracker/DataEntry/AddTransaction/AddIncomeVM.cs
@@ -0,0 +1,44 @@
+﻿using DLPMoneyTracker.Core;
+using DLPMoneyTracker.Data;
+using DLPMoneyTracker.Data.ConfigModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DLPMoneyTracker.DataEntry.AddTransaction
+{
+    public class AddIncomeVM : BaseViewModel
+    {
+        private ILedger _ledger;
+        private ITrackerConfig _config;
+
+
+        private DateTime _dateTrans;
+
+        public DateTime TransactionDate
+        {
+            get { return _dateTrans; }
+            set 
+            {
+                _dateTrans = value;
+                NotifyPropertyChanged(nameof(this.TransactionDate));
+            }
+        }
+
+
+
+
+
+
+        private List<SpecialDropListItem<MoneyAccount>> _listBanks = new List<SpecialDropListItem<MoneyAccount>>();
+        public List<SpecialDropListItem<MoneyAccount>> BankAccountList { get { return _listBanks; } }
+
+
+        public AddIncomeVM(ITrackerConfig config, ILedger ledger) : base()
+        {
+            _ledger = ledger;
+            _config = config;
+        }
+
+    }
+}
