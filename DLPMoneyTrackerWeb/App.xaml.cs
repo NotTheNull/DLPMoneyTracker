3cfa7b78cfb8ab64955294b90075035c0bbe9f8d
Landon Peery <objnotnull@live.com>
2023-04-23 10:11:33 -0700

    Getting started with .NET MAUI

---
+++ DLPMoneyTrackerWeb/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DLPMoneyTrackerWeb
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ DLPMoneyTrackerWeb/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DLPMoneyTrackerWeb
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
