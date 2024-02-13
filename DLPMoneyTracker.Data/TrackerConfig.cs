0665df286c9dbfe8062c9d739289ca65f0921429
NotTheNull <objnotnull@live.com>
2024-02-13 08:14:39 -0700

    Was missing some Special Accounts from the journal accounts listing

--- DLPMoneyTracker.Data/TrackerConfig.cs
+++ DLPMoneyTracker.Data/TrackerConfig.cs
@@ -44,14 +44,6 @@ namespace DLPMoneyTracker.Data
 
     public interface ITrackerConfig : IDisposable
     {
-        //[Obsolete]
-        //[EditorBrowsable(EditorBrowsableState.Never)]
-        //ReadOnlyCollection<MoneyAccount> AccountsList { get; }
-        //[Obsolete]
-        //ReadOnlyCollection<TransactionCategory> CategoryList { get; }
-
-        //ReadOnlyCollection<IJournalAccount> LedgerAccountsList { get; }
-
         void LoadFromFile(int year);
 
         void AddJournalAccount(IJournalAccount act);
@@ -65,46 +57,6 @@ namespace DLPMoneyTracker.Data
         IJournalAccount GetJournalAccount(Guid accountId);
 
         IEnumerable<IJournalAccount> GetJournalAccountList(JournalAccountSearch search);
-
-        //[Obsolete]
-        //[EditorBrowsable(EditorBrowsableState.Never)]
-        //void LoadMoneyAccounts();
-
-        //[Obsolete]
-        //[EditorBrowsable(EditorBrowsableState.Never)]
-        //void SaveMoneyAccounts();
-
-        //[Obsolete]
-        //[EditorBrowsable(EditorBrowsableState.Never)]
-        //void LoadCategories();
-
-        //[Obsolete]
-        //[EditorBrowsable(EditorBrowsableState.Never)]
-        //void SaveCategories();
-
-        //[Obsolete]
-        //[EditorBrowsable(EditorBrowsableState.Never)]
-        //TransactionCategory GetCategory(Guid uid);
-
-        //[Obsolete]
-        //[EditorBrowsable(EditorBrowsableState.Never)]
-        //MoneyAccount GetAccount(string id);
-
-        //[Obsolete]
-        //[EditorBrowsable(EditorBrowsableState.Never)]
-        //void AddCategory(TransactionCategory cat);
-
-        //[Obsolete]
-        //[EditorBrowsable(EditorBrowsableState.Never)]
-        //void AddMoneyAccount(MoneyAccount act);
-
-        //[Obsolete]
-        //[EditorBrowsable(EditorBrowsableState.Never)]
-        //void RemoveCategory(TransactionCategory cat);
-
-        //[Obsolete]
-        //[EditorBrowsable(EditorBrowsableState.Never)]
-        //void RemoveMoneyAccount(MoneyAccount act);
 
         void Copy(ITrackerConfig config);
 
@@ -120,26 +72,9 @@ namespace DLPMoneyTracker.Data
         private string LedgerAccountsConfig
         { get { return string.Concat(this.FolderPath, "LedgerAccounts.json"); } }
 
-        //#region Obsolete Objects
-
-        //private string AccountListConfig { get { return string.Concat(this.FolderPath, "MoneyAccounts.json"); } }
-
-        //[Obsolete("Switch to ILedger Accounts")]
-        //private List<MoneyAccount> _listAccts = new List<MoneyAccount>();
-        //[Obsolete("Switch to ILedger Accounts")]
-        //public ReadOnlyCollection<MoneyAccount> AccountsList { get { return _listAccts.OrderBy(o => o.ID).ToList().AsReadOnly(); } }
-
-        //private string CategoryListConfig { get { return string.Concat(this.FolderPath, "Categories.json"); } }
-
-        //[Obsolete("Switch to ILedger Accounts")]
-        //private List<TransactionCategory> _listCategories = new List<TransactionCategory>();
-        //[Obsolete("Switch to ILedger Accounts")]
-        //public ReadOnlyCollection<TransactionCategory> CategoryList { get { return _listCategories.OrderBy(o => o.Name).ToList().AsReadOnly(); } }
-        //#endregion
 
         private List<IJournalAccount> _listLedgerAccounts = new List<IJournalAccount>();
 
-        //public ReadOnlyCollection<IJournalAccount> LedgerAccountsList { get { return _listLedgerAccounts.AsReadOnly(); } }
 
         public TrackerConfig() : this(DateTime.Today.Year)
         {
@@ -203,6 +138,8 @@ namespace DLPMoneyTracker.Data
             _listLedgerAccounts.Clear();
             _listLedgerAccounts.Add(SpecialAccount.InitialBalance);
             _listLedgerAccounts.Add(SpecialAccount.UnlistedAdjusment);
+            _listLedgerAccounts.Add(SpecialAccount.DebtInterest);
+            _listLedgerAccounts.Add(SpecialAccount.DebtReduction);
 
             if (File.Exists(LedgerAccountsConfig))
             {
@@ -247,108 +184,8 @@ namespace DLPMoneyTracker.Data
             _listLedgerAccounts.Add(act);
         }
 
-#pragma warning disable CS0612 // Type or member is obsolete
-#pragma warning disable CS0618 // Type or member is obsolete
-
-        /// <summary>
-        /// Converts Money Accounts and Transaction Categories into Ledger Accounts
-        /// </summary>
-        public void Convert()
-        {
-            // Conversion is complete
-            return;
-
-            //if (_listAccts?.Any() == true)
-            //{
-            //    var loopAccounts = _listAccts.ToList();
-            //    foreach (var act in loopAccounts)
-            //    {
-            //        // If the account already exists, still need to remove the legacy
-            //        if (!LedgerAccountsList.Any(x => x.AccountType != MoneyAccountType.NotSet && x.MoneyAccountId == act.ID))
-            //        {
-            //            switch (act.AccountType)
-            //            {
-            //                case MoneyAccountType.Checking:
-            //                case MoneyAccountType.Savings:
-            //                    _listLedgerAccounts.Add(new BankAccount(act));
-            //                    break;
-            //                case MoneyAccountType.CreditCard:
-            //                    _listLedgerAccounts.Add(new CreditCardAccount(act));
-            //                    break;
-            //                case MoneyAccountType.Loan:
-            //                    _listLedgerAccounts.Add(new LoanAccount(act));
-            //                    break;
-            //            }
-
-            //        }
-
-            //        // Do Not remove accounts until the Ledger can be converted
-            //        //_listAccts.Remove(act);
-            //    }
-
-            //    SaveMoneyAccounts();
-            //}
-
-            //if (_listCategories?.Any() == true)
-            //{
-            //    var loopCategories = _listCategories.ToList();
-            //    foreach(var cat in loopCategories)
-            //    {
-            //        // If the category already exists, still need to remove the legacy
-            //        if(!LedgerAccountsList.Any(x => x.AccountType == MoneyAccountType.NotSet && x.CategoryId == cat.ID))
-            //        {
-            //            switch(cat.CategoryType)
-            //            {
-            //                case CategoryType.Expense:
-            //                    PayableAccount ap = new PayableAccount(cat);
-            //                    _listLedgerAccounts.Add(ap);
-            //                    break;
-            //                case CategoryType.Income:
-            //                    ReceivableAccount ar = new ReceivableAccount(cat);
-            //                    _listLedgerAccounts.Add(ar);
-            //                    break;
-            //                case CategoryType.UntrackedAdjustment:
-            //                    PayableAccount pay = new PayableAccount(cat);
-            //                    pay.Description = string.Format("AP {0}", pay.Description);
-            //                    _listLedgerAccounts.Add(pay);
-
-            //                    ReceivableAccount rec = new ReceivableAccount(cat);
-            //                    rec.Description = string.Format("AR {0}", pay.Description);
-            //                    _listLedgerAccounts.Add(rec);
-            //                    break;
-            //            }
-
-            //        }
-            //        // Do Not remove accounts until the Ledger can be converted
-            //        //_listCategories.Remove(cat);
-            //    }
-            //    SaveCategories();
-            //}
-            //SaveJournalAccounts();
-        }
-
-#pragma warning restore CS0618 // Type or member is obsolete
-#pragma warning restore CS0612 // Type or member is obsolete
-
         public void Copy(ITrackerConfig config)
         {
-            //#pragma warning disable CS0618 // Type or member is obsolete
-            //#pragma warning disable CS0612 // Type or member is obsolete
-            //            // Until the conversion is done, this will need to stayu
-            //            _listAccts.Clear();
-            //            foreach (var act in config.AccountsList)
-            //            {
-            //                this.AddMoneyAccount(act);
-            //            }
-
-            //            _listCategories.Clear();
-            //            foreach (var cat in config.CategoryList)
-            //            {
-            //                this.AddCategory(cat);
-            //            }
-            //#pragma warning restore CS0612 // Type or member is obsolete
-            //#pragma warning restore CS0618 // Type or member is obsolete
-
             _listLedgerAccounts.Clear();
             JournalAccountSearch search = new JournalAccountSearch();
             search.JournalTypes.AddRange(new List<JournalAccountType>() { JournalAccountType.Bank, JournalAccountType.LiabilityCard, JournalAccountType.LiabilityLoan, JournalAccountType.Payable, JournalAccountType.Receivable });
@@ -363,21 +200,6 @@ namespace DLPMoneyTracker.Data
         public void Dispose()
         {
             GC.SuppressFinalize(this);
-
-            //#pragma warning disable CS0618 // Type or member is obsolete
-            //            // Will need to keep until conversion is done
-            //            if (_listAccts is not null)
-            //            {
-            //                _listAccts.Clear();
-            //                _listAccts = null;
-            //            }
-
-            //            if (_listCategories is not null)
-            //            {
-            //                _listCategories.Clear();
-            //                _listCategories = null;
-            //            }
-            //#pragma warning restore CS0618 // Type or member is obsolete
 
             if (_listLedgerAccounts is not null)
             {
