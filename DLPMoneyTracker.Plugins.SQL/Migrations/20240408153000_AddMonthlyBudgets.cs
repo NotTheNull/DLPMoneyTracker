0f172ba749193081e1dc6631e2c011e122bfff57
NotTheNull <objnotnull@live.com>
2024-04-08 08:30:45 -0700

    Updating database

---
+++ DLPMoneyTracker.Plugins.SQL/Migrations/20240408153000_AddMonthlyBudgets.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DLPMoneyTracker.Plugins.SQL.Migrations
+{
+    public partial class AddMonthlyBudgets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "CurrentBudget",
+                table: "Accounts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "DefaultBudget",
+                table: "Accounts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentBudget",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "DefaultBudget",
+                table: "Accounts");
+        }
+    }
+}
