6427f675daaa7be1cfb55147a73f9541ab037daf
NotTheNull <objnotnull@live.com>
2024-11-04 17:04:21 -0700

    Updated adapter to handle the CSVMapping

--- DLPMoneyTracker.Plugins.SQL/Adapters/SQLSourceToJournalAccountAdapter.cs
+++ DLPMoneyTracker.Plugins.SQL/Adapters/SQLSourceToJournalAccountAdapter.cs
@@ -3,6 +3,7 @@ using DLPMoneyTracker.BusinessLogic.PluginInterfaces;
 using DLPMoneyTracker.Core.Models;
 using DLPMoneyTracker.Core.Models.LedgerAccounts;
 using DLPMoneyTracker.Plugins.SQL.Data;
+using Microsoft.EntityFrameworkCore.Migrations.Operations.Builders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -47,8 +48,8 @@ namespace DLPMoneyTracker.Plugins.SQL.Adapters
             this.JournalType = cpy.JournalType;
             this.OrderBy = cpy.OrderBy;
             this.DateClosedUTC = cpy.DateClosedUTC;
-            
-            if(cpy is INominalAccount nominal)
+
+            if (cpy is INominalAccount nominal)
             {
                 this.BudgetType = nominal.BudgetType;
                 this.DefaultMonthlyBudgetAmount = nominal.DefaultMonthlyBudgetAmount;
@@ -62,7 +63,6 @@ namespace DLPMoneyTracker.Plugins.SQL.Adapters
             }
         }
 
-        // TODO: Finish integration of CSVMapping into SQL Database
         public void ExportSource(ref Account acct)
         {
             ArgumentNullException.ThrowIfNull(acct);
@@ -76,7 +76,36 @@ namespace DLPMoneyTracker.Plugins.SQL.Adapters
             acct.DefaultBudget = this.DefaultMonthlyBudgetAmount;
             acct.CurrentBudget = this.CurrentBudgetAmount;
 
+            if (this.Mapping?.Mapping.Count > 0)
+            {
+                if (acct.CSVMapping is null)
+                {
+                    acct.CSVMapping = new CSVMain()
+                    {
+                        AccountId = acct.Id
+                    };
+                }
+                acct.CSVMapping.IsAmountInverted = this.Mapping.IsAmountInverted;
+                acct.CSVMapping.StartingRow = this.Mapping.StartingRow;
+
+                foreach(var key in this.Mapping.Mapping.Keys)
+                {
+                    var column = acct.CSVMapping.Columns.FirstOrDefault(x => x.ColumnName == key);
+                    if(column is null)
+                    {
+                        column = new CSVColumn()
+                        {
+                            MainId = acct.CSVMapping.Id,
+                            ColumnName = key
+                        };
+                        acct.CSVMapping.Columns.Add(column);
+                    }
+                    column.ColumnIndex = this.Mapping.Mapping[key];
+                    
+                }
+            }
         }
+
 
         public void ImportSource(Account acct)
         {
@@ -90,6 +119,27 @@ namespace DLPMoneyTracker.Plugins.SQL.Adapters
             this.BudgetType = acct.BudgetType;
             this.DefaultMonthlyBudgetAmount = acct.DefaultBudget;
             this.CurrentBudgetAmount = acct.CurrentBudget;
+
+            this.Mapping = null;
+            if(acct.CSVMapping != null)
+            {
+                this.Mapping = new CSVMapping()
+                {
+                    IsAmountInverted = acct.CSVMapping.IsAmountInverted,
+                    StartingRow = acct.CSVMapping.StartingRow
+                };
+
+                foreach(var col in acct.CSVMapping.Columns)
+                {
+                    if(this.Mapping.Mapping.ContainsKey(col.ColumnName))
+                    {
+                        this.Mapping.Mapping[col.ColumnName] = col.ColumnIndex;
+                    } else
+                    {
+                        this.Mapping.Mapping.Add(col.ColumnName, col.ColumnIndex);
+                    }
+                }
+            }
         }
 
 
