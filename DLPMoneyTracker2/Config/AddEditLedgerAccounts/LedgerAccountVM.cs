87c496a3cc1724ccf8dfaee77ac6e49437353528
NotTheNull <objnotnull@live.com>
2024-11-17 09:58:56 -0700

    Make sure that a proper GUID is assigned on init

--- DLPMoneyTracker2/Config/AddEditLedgerAccounts/LedgerAccountVM.cs
+++ DLPMoneyTracker2/Config/AddEditLedgerAccounts/LedgerAccountVM.cs
@@ -24,6 +24,7 @@ namespace DLPMoneyTracker2.Config.AddEditLedgerAccounts
         {
             this.getNextUIDUseCase = getNextUIDUseCase;
             this.saveUseCase = saveUseCase;
+            this.Clear();
         }
 
         private IJournalAccount _mainAccount;
