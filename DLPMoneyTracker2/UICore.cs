8f768861decc3ae245eaf971c0a32a89d669b32a
NotTheNull <objnotnull@live.com>
2024-11-17 10:58:00 -0700

    Updated to separate Live and Test config files

--- DLPMoneyTracker2/UICore.cs
+++ DLPMoneyTracker2/UICore.cs
@@ -39,7 +39,7 @@ namespace DLPMoneyTracker2
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public static IServiceProvider DependencyHost { get; set; }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-
+                
         public static void Init()
         {
             ServiceCollection services = new ServiceCollection();
