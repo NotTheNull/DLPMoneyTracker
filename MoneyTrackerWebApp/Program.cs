2d42b18ec999fb33a5eb2ff402968a4b28a2e5cf
NotTheNull <objnotnull@live.com>
2024-11-25 16:20:34 -0700

    Added logging to project

--- MoneyTrackerWebApp/Program.cs
+++ MoneyTrackerWebApp/Program.cs
@@ -1,5 +1,6 @@
 using MoneyTrackerWebApp;
 using MoneyTrackerWebApp.Components;
+using MoneyTrackerWebApp.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddMyConsoleLogger();
 
 
 SetupIOC.Configure(builder);
