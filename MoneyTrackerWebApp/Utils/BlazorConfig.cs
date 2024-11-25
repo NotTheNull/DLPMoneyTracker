c6c358fb34bd7af5c16a84b9aa37b4d3193d989f
NotTheNull <objnotnull@live.com>
2024-11-25 16:19:24 -0700

    Moved to Utils folder for improved organization clarity

--- MoneyTrackerWebApp/Utils/BlazorConfig.cs
+++ MoneyTrackerWebApp/Utils/BlazorConfig.cs
@@ -1,18 +1,18 @@
 ﻿using DLPMoneyTracker.Core;
 
-namespace MoneyTrackerWebApp.Models.Core
+namespace MoneyTrackerWebApp.Utils
 {
     public class BlazorConfig : IDLPConfig
     {
 
         public BlazorConfig(IConfiguration config)
         {
-            this.DataSource = config.GetValue<string>("AppSettings:source")?.ToDataSource() ?? DLPDataSource.NotSet;
-            
-            string connName = config.GetValue<string>("AppSettings:connName") ?? string.Empty;
-            this.DBConnectionString = config.GetConnectionString(connName) ?? string.Empty;
+            DataSource = config.GetValue<string>("AppSettings:source")?.ToDataSource() ?? DLPDataSource.NotSet;
 
-            this.JSONFilePath = config.GetConnectionString("json_path") ?? Directory.GetCurrentDirectory();
+            string connName = config.GetValue<string>("AppSettings:connName") ?? string.Empty;
+            DBConnectionString = config.GetConnectionString(connName) ?? string.Empty;
+
+            JSONFilePath = config.GetConnectionString("json_path") ?? Directory.GetCurrentDirectory();
         }
 
 
