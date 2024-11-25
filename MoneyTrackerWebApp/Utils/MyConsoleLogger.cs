b53d145a8144689445606e4300b49aec97f0c581
NotTheNull <objnotnull@live.com>
2024-11-25 16:20:00 -0700

    Added custom Console logger to circumvent isue with Blazor's standard Console logger

---
+++ MoneyTrackerWebApp/Utils/MyConsoleLogger.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+
+namespace MoneyTrackerWebApp.Utils
+{
+    internal class MyConsoleLogger<T> : MyConsoleLogger, ILogger<T>
+    {
+        public MyConsoleLogger() : base(typeof(T).FullName, null) { }
+        public MyConsoleLogger(Func<string, LogLevel, bool> filter) : base(typeof(T).FullName, filter) { }
+    }
+
+
+    internal class MyConsoleLogger : ILogger
+    {
+        public MyConsoleLogger(string name, Func<string, LogLevel, bool> filter) 
+        {
+            this.Name = name ?? throw new ArgumentNullException(nameof(name));
+            this.Filter = filter ?? ((category, logLevel) => true);
+        }
+
+        public Func<string, LogLevel, bool> Filter { get; set; }
+        public string Name { get; }
+
+        public bool IsEnabled(LogLevel lvl)
+        {
+            return lvl != LogLevel.None && this.Filter(this.Name, lvl);
+        }
+
+        public IDisposable BeginScope<TState>(TState state) => null;
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            if (!this.IsEnabled(logLevel)) return;
+
+            string message = formatter != null ? formatter(state, exception) : state.ToString();
+            Console.WriteLine($"[{this.Name}]: {message}");
+            if(exception != null)
+            {
+                Console.Error.WriteLine(exception.ToString());
+            }
+
+        }
+    }
+}
---
+++ MoneyTrackerWebApp/Utils/MyConsoleLoggerProvider.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Collections.Concurrent;
+
+namespace MoneyTrackerWebApp.Utils
+{
+    // NOTE: Code copied from [https://github.com/dotnet/aspnetcore/issues/20504]
+
+    internal class MyConsoleLoggerProvider : ILoggerProvider
+    {
+        private static readonly Func<string, LogLevel, bool> DEFAULT_FILTER = (cat, level) => true;
+
+        private readonly Func<string, LogLevel, bool> _filter;
+        private ConcurrentDictionary<string, MyConsoleLogger> _listLoggers = new ConcurrentDictionary<string, MyConsoleLogger>();
+        
+        public MyConsoleLoggerProvider(Func<string, LogLevel, bool> filter)
+        {
+            _filter = filter ?? throw new ArgumentNullException(nameof(filter));
+        }
+
+        public Func<string, LogLevel, bool> Filter { get { return _filter ?? DEFAULT_FILTER; } }
+
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            if (string.IsNullOrWhiteSpace(categoryName)) throw new ArgumentNullException(nameof(categoryName));
+
+            return _listLoggers.GetOrAdd(categoryName, new MyConsoleLogger(categoryName, this.Filter));
+        }
+
+
+        public void Dispose()
+        {
+            _listLoggers?.Clear();
+        }
+    }
+
+    internal static class MyConsoleLoggerConfiguration
+    {
+        public static ILoggingBuilder AddMyConsoleLogger(this ILoggingBuilder builder)
+        {
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, MyConsoleLoggerProvider>());
+
+            // HACK: Replace the Blazor built-in logger (which fails to log to the console) with this provider
+            builder.Services.Add(ServiceDescriptor.Singleton(typeof(ILogger<>), typeof(MyConsoleLogger<>)));
+
+            return builder;
+        }
+
+    }
+}
