62c05fafeae60579d12dae50dc5715e606bf3ea9
NotTheNull <objnotnull@live.com>
2025-01-06 10:00:32 -0700

    Ability to modify Ledger Accounts finished.  Will need to rebase to get newly added features

---
+++ MoneyTrackerWebApp/Components/Pages/Config/LedgerAccounts/EditLedgerAccountBase.cs
@@ -0,0 +1,108 @@
+﻿using DLPMoneyTracker.Core;
+using DLPMoneyTracker.Core.Models.LedgerAccounts;
+using Microsoft.AspNetCore.Components;
+using MoneyTrackerWebApp.Components.Pages.Config.MoneyAccounts;
+using MoneyTrackerWebApp.Models.Config.LedgerAccounts;
+using MoneyTrackerWebApp.Models.Core;
+using MoneyTrackerWebApp.Services;
+
+namespace MoneyTrackerWebApp.Components.Pages.Config.LedgerAccounts
+{
+    public class EditLedgerAccountBase : ComponentBase
+    {
+        [Parameter]
+        public Guid? AccountUID { get; set; }
+
+        [Inject]
+        public IJournalAccountService AccountService { get; set; }
+
+        [Inject]
+        public StorageService<IJournalAccount>  Storage { get; set; }
+
+        [Inject]
+        public INavigationHistoryService Navigation { get; set; }
+
+        [Inject]
+        public ILogger<EditMoneyAccountBase> Logger { get; set; }
+
+
+        protected readonly BudgetTrackingType[] listBudgetTypes = [BudgetTrackingType.DO_NOT_TRACK, BudgetTrackingType.Fixed, BudgetTrackingType.Variable];
+        protected readonly LedgerType[] listLedgerTypes = [LedgerType.Payable, LedgerType.Receivable];
+        protected EditLedgerAccountVM Account { get; set; } = new EditLedgerAccountVM();
+
+        private readonly string URL_ACCOUNTLIST = "/config/ledgeraccounts";
+
+        protected override void OnParametersSet()
+        {
+            Logger.LogInformation("Setting parameters");
+            if (AccountUID is null || AccountUID == Guid.Empty) return;
+
+            Logger.LogInformation($"Loading account with UID {this.AccountUID}");
+            var acct = AccountService.GetAccount(this.AccountUID.Value);
+            this.Storage.Data = acct;
+            this.Account.Copy(acct);
+        }
+
+        protected override void OnInitialized()
+        {
+            Logger.LogInformation("Initializing");
+            if (this.Storage.Data == null) return;
+
+            Logger.LogInformation("Copying account information from storage");
+            this.Account.Copy(this.Storage.Data);
+        }
+
+        #region Form Events
+
+
+        protected void OnLedgerTypeChanged(ChangeEventArgs e)
+        {
+            Account.JournalType = (LedgerType)e.Value;
+        }
+
+        protected void OnDescriptionChanged(ChangeEventArgs e)
+        {
+            Account.Description = e.Value.ToString();
+        }
+
+        protected void OnBudgetTypeChanged(ChangeEventArgs e)
+        {
+            Account.BudgetType = (BudgetTrackingType)e.Value;
+        }
+
+        protected void OnDefaultBudgetAmountChanged(ChangeEventArgs e)
+        {
+            Account.DefaultMonthlyBudgetAmount = e.Value?.ToString().ToDecimal() ?? decimal.Zero;
+        }
+
+
+
+        #endregion
+
+
+        protected void SaveChanges()
+        {
+            AccountService.SaveAccount(Account);
+            this.ReturnToList();
+        }
+
+        protected void Reset()
+        {
+            if(Storage.Data != null)
+            {
+                Account.Copy(Storage.Data);
+            } else
+            {
+                this.ReturnToList();
+            }
+        }
+
+        protected void ReturnToList()
+        {
+            this.Storage.Data = null;
+            Navigation.NavigateBack(URL_ACCOUNTLIST);
+        }
+
+
+    }
+}
---
+++ MoneyTrackerWebApp/Models/Config/LedgerAccounts/EditLedgerAccountVM.cs
@@ -0,0 +1,56 @@
+﻿using DLPMoneyTracker.Core.Models.LedgerAccounts;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoneyTrackerWebApp.Models.Config.LedgerAccounts
+{
+    public class EditLedgerAccountVM : INominalAccount
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(200)]
+        [MinLength(3, ErrorMessage = "Description must have at least 3 characters")]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public LedgerType JournalType { get; set; } = LedgerType.NotSet;
+
+        public BudgetTrackingType BudgetType { get; set; } = BudgetTrackingType.DO_NOT_TRACK;
+
+        [Range(0.0d, 99999.99d)]
+        public decimal DefaultMonthlyBudgetAmount { get; set; } = decimal.Zero;
+
+        public decimal CurrentBudgetAmount { get; } = decimal.Zero;
+
+
+        [Range(1, 99, ErrorMessage = "Display order must be greater than 1")]
+        public int OrderBy { get; set; } = 0;
+
+        public DateTime? DateClosedUTC { get; set; } = null;
+
+        
+
+
+
+
+
+
+        public void Copy(IJournalAccount cpy)
+        {
+            if (cpy is null) return;
+
+            this.Id = cpy.Id;
+            this.Description = cpy.Description;
+            this.JournalType = cpy.JournalType;
+            this.OrderBy = cpy.OrderBy;
+            this.DateClosedUTC = cpy.DateClosedUTC;
+            
+            if(cpy is INominalAccount acc)
+            {
+                this.BudgetType = acc.BudgetType;
+                this.DefaultMonthlyBudgetAmount = acc.DefaultMonthlyBudgetAmount;
+                
+            }
+        }
+    }
+}
