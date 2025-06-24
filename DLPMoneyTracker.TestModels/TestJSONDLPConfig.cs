13e9a4013d1f391756ffbe694b8eee9c06a8216b
NotTheNull <objnotnull@live.com>
2025-06-23 18:00:57 -0700

    Adding Test JSON config

---
+++ DLPMoneyTracker.TestModels/TestJSONDLPConfig.cs
@@ -0,0 +1,14 @@
+﻿using DLPMoneyTracker.Core;
+
+namespace DLPMoneyTracker.TestModels
+{
+    public class TestJSONDLPConfig : IDLPConfig
+    {
+        public DLPDataSource DataSource => DLPDataSource.JSON;
+
+        public string DBConnectionString => string.Empty;
+
+        private const string TMP_PATH = "C:\\Users\\crc\\AppData\\Local\\Temp";
+        public string JSONFilePath => Path.Combine(TMP_PATH, "MoneyTrackerTest");
+    }
+}
