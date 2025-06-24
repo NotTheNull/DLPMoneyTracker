823e4f13aef8adcb84ad793d2bc772f0740e8103
NotTheNull <objnotnull@live.com>
2025-06-23 19:00:16 -0700

    Basics of Class Fixture set up

---
+++ DLPMoneyTracker.Plugins.JSON.Tests/Fixture/JSONFixture.cs
@@ -0,0 +1,44 @@
+﻿using DLPMoneyTracker.BusinessLogic.PluginInterfaces;
+using DLPMoneyTracker.Core;
+using DLPMoneyTracker.Plugins.JSON.Repositories;
+using DLPMoneyTracker.TestModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLPMoneyTracker.Plugins.JSON.Tests.Fixture
+{
+    internal class JSONFixture : IDisposable
+    {
+
+        private readonly IServiceProvider _serviceProvider;
+        public JSONFixture()
+        {
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton<IDLPConfig, TestJSONDLPConfig>();
+                    services.AddSingleton<ILedgerAccountRepository, JSONLedgerAccountRepository>();
+                })
+                .Build();
+            _serviceProvider = host.Services;
+                
+        }
+
+
+        public T GetService<T>() where T : class
+        {
+            return _serviceProvider.GetRequiredService<T>();
+        }
+
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
