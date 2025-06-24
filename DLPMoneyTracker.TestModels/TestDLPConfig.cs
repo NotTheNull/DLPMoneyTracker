a39a1b6492ab2f9de555348f4f70d35aeaa1d3a1
NotTheNull <objnotnull@live.com>
2025-06-23 17:54:30 -0700

    Setting up a Project for shared testing objects, particularly reusable stubs

---
+++ DLPMoneyTracker.TestModels/TestDLPConfig.cs
@@ -0,0 +1,13 @@
+﻿using DLPMoneyTracker.Core;
+
+namespace DLPMoneyTracker.TestModels
+{
+    public class TestDLPConfig : IDLPConfig
+    {
+        public DLPDataSource DataSource { get; set; } = DLPDataSource.NotSet;
+
+        public string DBConnectionString { get; set; } = string.Empty;
+
+        public string JSONFilePath { get; set; } = string.Empty;
+    }
+}
