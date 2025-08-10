db8cde7e5dc689618d56033e126013e6012b0f23
NotTheNull <objnotnull@live.com>
2025-08-10 11:34:56 -0700

    Installed AutoMapper nuget

--- DLPMoneyTracker.Core/DateRange.cs
+++ DLPMoneyTracker.Core/DateRange.cs
@@ -1,4 +1,6 @@
-﻿namespace DLPMoneyTracker.Core
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace DLPMoneyTracker.Core
 {
     public class DateRange
     {
@@ -16,22 +18,14 @@
         /// </summary>
         /// <param name="begin"></param>
         /// <param name="end"></param>
-        public DateRange(DateTime begin, DateTime end)
-        {
-            this.Begin = begin;
-            this.End = end;
-        }
+        public DateRange(DateTime begin, DateTime end) => GetRange(begin, end);
 
         /// <summary>
         /// Sets the Date Range for the full month of the given year
         /// </summary>
         /// <param name="year"></param>
         /// <param name="month"></param>
-        public DateRange(int year, int month)
-        {
-            this.Begin = new DateTime(year, month, 1);
-            this.End = new DateTime(year, month, DateTime.DaysInMonth(year, month)).AddDays(1).AddMilliseconds(-1);
-        }
+        public DateRange(int year, int month) => GetMonthRange(year, month);
 
         public bool IsWithinRange(DateTime date)
         {
@@ -44,5 +38,13 @@
 
             return false;
         }
+
+        public static DateRange GetMonthRange(int year, int month) => new()
+        {
+            Begin = new DateTime(year, month, 1),
+            End = new DateTime(year, month, DateTime.DaysInMonth(year, month)).AddDays(1).AddMilliseconds(-1)
+        };
+
+        public static DateRange GetRange(DateTime begin, DateTime end) => new() { Begin = begin, End = end };
     }
 }
